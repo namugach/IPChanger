ea88ba3ac365badf6648757208c8ba25ccdc8a8f
namugach <namugach@gmail.com>
2024-05-16 00:56:57 +0900

    Add IP 변경 프로그램에 IP 유효성 검사
    - `ip_changer.py` 스크립트에 `is_valid_ip()` 함수 추가
    - 사용자가 유효하지 않은 IP 주소를 입력했을 때 재입력 요청 기능 추가

--- main.py
+++ main.py
@@ -12,31 +12,41 @@ ssh_config_path = os.path.expanduser(ssh_config_path)
 def find_old_ip_in_file(file_path):
 	# 파일 읽기
 	with open(file_path, 'r') as file:
-		content = file.read()
+			content = file.read()
 	
 	# IP 주소 찾기 (IPv4 주소 패턴 사용)
 	ip_pattern = re.compile(r'(\d{1,3}\.){3}\d{1,3}')
 	match = ip_pattern.search(content)
 	
 	if match:
-		return match.group()
+			return match.group()
 	else:
-		raise ValueError(f'파일 {file_path}에서 유효한 IP 주소를 찾을 수 없어.')
+			raise ValueError(f'파일 {file_path}에서 유효한 IP 주소를 찾을 수 없어.')
+
+def is_valid_ip(ip):
+	# IP 주소 유효성 검사 (IPv4 형식)
+	ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
+	return ip_pattern.match(ip) is not None
 
 def update_ip_in_file(file_path, old_ip, new_ip):
 	# 파일 읽기
 	with open(file_path, 'r') as file:
-		content = file.read()
+			content = file.read()
 	
 	# IP 주소 대체
 	new_content = re.sub(re.escape(old_ip), new_ip, content)
 
 	# 파일 쓰기
 	with open(file_path, 'w') as file:
-		file.write(new_content)
+			file.write(new_content)
 
 # 사용자로부터 새로운 IP 입력 받기
-new_ip = input('새로운 IP 주소를 입력하세요: ')
+while True:
+	new_ip = input('새로운 IP 주소를 입력하세요: ')
+	if is_valid_ip(new_ip):
+			break
+	else:
+			print('유효하지 않은 IP 주소입니다. 다시 입력해주세요.')
 
 # 각 파일에서 예전 IP 주소 찾기
 try:
